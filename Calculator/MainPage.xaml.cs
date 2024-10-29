f274561138bfff48b0f6aea6ee7bdcadd42456cf
school <User-20@DESKTOP-UHL3TVA>
2024-10-29 15:56:51 +0200

    yesss

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -21,48 +21,33 @@ namespace Calculator
     
     public sealed partial class MainPage : Page
     {
-        private bool opPressed = false;
-        private int fNum =0;
-        private int sNum =0;
-        private char oper;
+        private double answer;
+        private double _num1;
+        private double _num2;
+        private char _operator = '@';
         public MainPage()
         {
             this.InitializeComponent();
 
         }
-        public void OpButtonClick(char op)
-        {
-            opPressed = true;
-            oper = op;
-        }
-
-        public void DigButtonClick(int dig)
-        {
-            if (!opPressed)
-            {
-                fNum *= 10;
-                fNum += dig;
-                firstNumTXT.Text = fNum.ToString();
-            }
-            else if (opPressed)
-            {
-                sNum *= 10;
-                sNum += dig;
-                secNumTXT.Text = sNum.ToString();
-            }
-
-        }
+        
 
         
 
         private void oneBtn_Click_1(object sender, RoutedEventArgs e)
         {
-            DigButtonClick(1);
+            if(_operator =='@')
+                firstNumTXT.Text += "1";
+            else
+                secNumTXT.Text += "1";
         }
 
         private void twoBtn_Click(object sender, RoutedEventArgs e)
         {
-            DigButtonClick(2);
+            if (_operator == '@')
+                firstNumTXT.Text += "2";
+            else
+                secNumTXT.Text += "2";
         }
 
         private void queBtn_Click(object sender, RoutedEventArgs e)
@@ -79,6 +64,105 @@ namespace Calculator
         {
 
             helpImg.Source = new BitmapImage(new Uri("ms-appx:///Assets/Digits/helpBtn2.png"));
+        }
+
+        private void threeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "3";
+            else
+                secNumTXT.Text += "3";
+        }
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "4";
+            else
+                secNumTXT.Text += "4";
+        }
+
+        private void fiveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "5";
+            else
+                secNumTXT.Text += "5";
+        }
+
+        private void sixBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "6";
+            else
+                secNumTXT.Text += "6";
+        }
+
+        private void sevenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "7";
+            else
+                secNumTXT.Text += "7";
+        }
+
+        private void eightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "8";
+            else
+                secNumTXT.Text += "8";
+        }
+
+        private void nineBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "9";
+            else
+                secNumTXT.Text += "9";
+        }
+
+        private void ansBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zeroBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_operator == '@')
+                firstNumTXT.Text += "0";
+            else
+                secNumTXT.Text += "0";
+        }
+
+        private void queBtn_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            helpImg.Source = new BitmapImage(new Uri("ms-appx:///Assets/Digits/helpBtn1.png"));
+        }
+
+        private void divBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _operator = '/';
+        }
+
+        private void mulBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _operator = '*';
+        }
+
+        private void plusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _operator = '+';
+        }
+
+        private void minusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _operator = '-';
         }
     }
     
