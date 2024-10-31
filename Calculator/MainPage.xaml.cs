edc5edb8a629552573e5499f7438a28a93b27bd2
נבו גוטליב <gotlivnevo415@gmail.com>
2024-10-31 23:26:09 +0200

    finished but question button

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -21,33 +21,43 @@ namespace Calculator
     
     public sealed partial class MainPage : Page
     {
-        private double answer;
-        private double _num1;
-        private double _num2;
+        private double _answer=0;
+        private double _num1=0;
+        private double _num2=0;
         private char _operator = '@';
+        private bool isNeg = false;
         public MainPage()
         {
             this.InitializeComponent();
 
         }
         
-
+        private void ClearALl()
+        {
+            _num1 = 0;
+            _num2 = 0;
+            _operator = '@';
+            firstNumTXT.Text = "";
+            secNumTXT.Text = "";
+        }
+        private Windows.UI.Xaml.Controls.TextBlock Ctextbox()
+        {
+            if (_operator == '@')
+                return firstNumTXT;
+            return secNumTXT;
+        }
         
 
         private void oneBtn_Click_1(object sender, RoutedEventArgs e)
         {
-            if(_operator =='@')
-                firstNumTXT.Text += "1";
-            else
-                secNumTXT.Text += "1";
+        
+            Ctextbox().Text += "1";
         }
 
         private void twoBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "2";
-            else
-                secNumTXT.Text += "2";
+            
+            Ctextbox().Text += "2";
         }
 
         private void queBtn_Click(object sender, RoutedEventArgs e)
@@ -55,10 +65,7 @@ namespace Calculator
 
         }
 
-        private void queBtn_PointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-
-        }
+      
 
         private void queBtn_PointerEntered_1(object sender, PointerRoutedEventArgs e)
         {
@@ -68,76 +75,65 @@ namespace Calculator
 
         private void threeBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "3";
-            else
-                secNumTXT.Text += "3";
+            
+             Ctextbox().Text += "3";
         }
 
         private void clearBtn_Click(object sender, RoutedEventArgs e)
         {
+            ansBox.Text = "";
+            _answer = 0;
+            ClearALl();
 
+            
         }
 
         private void fourBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "4";
-            else
-                secNumTXT.Text += "4";
+            
+            Ctextbox().Text += "4";
         }
 
         private void fiveBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "5";
-            else
-                secNumTXT.Text += "5";
+
+            Ctextbox().Text += "5";
         }
 
         private void sixBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "6";
-            else
-                secNumTXT.Text += "6";
+
+            Ctextbox().Text += "6";
         }
 
         private void sevenBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "7";
-            else
-                secNumTXT.Text += "7";
+            
+            Ctextbox().Text += "7";
         }
 
         private void eightBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "8";
-            else
-                secNumTXT.Text += "8";
+            
+            Ctextbox().Text += "8";
         }
 
         private void nineBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "9";
-            else
-                secNumTXT.Text += "9";
+
+            Ctextbox().Text += "9";
         }
 
         private void ansBtn_Click(object sender, RoutedEventArgs e)
         {
 
+            Ctextbox().Text += _answer;
         }
 
         private void zeroBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (_operator == '@')
-                firstNumTXT.Text += "0";
-            else
-                secNumTXT.Text += "0";
+            
+            Ctextbox().Text += "0";
         }
 
         private void queBtn_PointerExited(object sender, PointerRoutedEventArgs e)
@@ -164,6 +160,93 @@ namespace Calculator
         {
             _operator = '-';
         }
+
+        private void equBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _num1 = double.Parse(firstNumTXT.Text);
+            _num2 = double.Parse(secNumTXT.Text);
+            switch(_operator)
+            {
+                case '+':
+                    _answer = _num1 + _num2;
+                    ansBox.Text = _answer.ToString();
+                    ClearALl();
+                    break;
+                case '*':
+                    _answer = _num1 * _num2;
+                    ansBox.Text = _answer.ToString();
+                    ClearALl();
+                    break;
+                case '-':
+                    _answer = _num1 - _num2;
+                    ansBox.Text = _answer.ToString();
+                    ClearALl();
+                    break;
+                case '/':
+                    if(_num2!=0) { 
+                        _answer = _num1 + _num2;
+                        
+                        ansBox.Text = _answer.ToString();
+                        ClearALl();
+
+                    }
+                    else
+                    {
+                        ansBox.Text = "You cant divide in 0!";
+                    }
+                    break;
+                case 's':
+                    _answer = Math.Sin(_num2);
+                    ansBox.Text = _answer.ToString();
+                    ClearALl();
+                    break;
+                case 't':
+                    _answer = Math.Tan(_num2);
+                    ansBox.Text = _answer.ToString();
+                    ClearALl();
+                    break;
+                case 'c':
+                    _answer = Math.Cos(_num2);
+                    ansBox.Text = _answer.ToString();
+                    ClearALl();
+                    break;
+            }
+        }
+
+        
+
+        private void backBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            Ctextbox().Text = secNumTXT.Text.ToString().Substring(0, secNumTXT.Text.ToString().Length - 1);
+        }
+
+        private void negBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(!isNeg)
+                Ctextbox().Text = '-'+Ctextbox().Text.ToString().Substring(0, Ctextbox().Text.ToString().Length);
+            else
+
+        }
+
+        private void dotBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Ctextbox().Text += '.';
+        }
+
+        private void sinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _operator = 's';
+        }
+        private void cosBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _operator = 'c';
+        }
+        private void tanBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _operator = 't';
+        }
+
+        
     }
     
 
